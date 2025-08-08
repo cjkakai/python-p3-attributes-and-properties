db33101b210db59ca8669e6d1722036321c70091
Julius Kakai <cjkakai5@gmail.com>
2025-08-08 15:26:14 +0300

    add  functions

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env python3
-
 APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
+    "Admin", "Customer Service", "Human Resources", "ITC", "Production",
+    "Legal", "Finance", "Sales", "General Management",
+    "Research & Development", "Marketing", "Purchasing"
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Admin"):
+        self.name = name  # goes through setter
+        self.job = job    # goes through setter
+
+    # ----- name property -----
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+
+    # ----- job property -----
+    def get_job(self):
+        return self._job
+
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+    job = property(get_job, set_job)
